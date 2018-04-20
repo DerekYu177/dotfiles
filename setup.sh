0a2d1ca47f2b5040c76f88dcedb7bc7dc1fc96ef
DerekYu <derek-nis@hotmail.com>
2018-04-19 20:39:30 -0400

    removing the print statments

--- setup.sh
+++ setup.sh
@@ -2,14 +2,8 @@
 
 # Make utilities available
 
-DOTFILES_DIR='~/.dotfiles'
+DOTFILES_DIR="$HOME/.dotfiles"
 PATH="$DOTFILES_DIR/bin:$PATH"
-
-# Update dotfiles itself first
-if is-executable git -a -d "$DOTFILES_DIR/.git" 
-then 
-  git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
-fi
 
 # Bunch of symlinks
 ln -sfv "$DOTFILES_DIR/core/bash_profile" "$HOME/.bash_profile"
